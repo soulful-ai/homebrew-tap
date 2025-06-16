d6eb7d7b997d17ffef9e53fc6abb24d40304e32c
soulful-admin <soulful@soulless.app>
2025-06-16 13:17:40 +0200

    Add charset-normalizer and idna dependencies to fix import errors

---
+++ Formula/chatmcp-cli-minimal.rb
@@ -0,0 +1,20 @@
+class ChatmcpCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "ChatMCP CLI - AI pair programming with MCP server integration"
+  homepage "https://github.com/soulful-ai/platforma"
+  url "https://files.pythonhosted.org/packages/5f/a7/c19d5eda0e65af8feafaf6b722bc6ad5cfa0cb86798a4a3d64a16003bf4c/chatmcp_cli-1.1.0.tar.gz"
+  sha256 "0585fd06a4f566bc2775255ba847537a488db52f3886f27a4be547fb79facaaf"
+  license "Apache-2.0"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_predicate bin/"chatmcp", :exist?
+    assert_predicate bin/"aider", :exist?
+  end
+end
--- Formula/chatmcp-cli.rb
+++ Formula/chatmcp-cli.rb
@@ -100,6 +100,15 @@ class ChatmcpCli < Formula
     sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
   end
 
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e4/33/89c2ced2b67d1c2a61c19c6751aa8902d46ce3dacb23600a283619f5a12d/charset_normalizer-3.4.2.tar.gz"
+    sha256 "5baececa9ecba31eff645232d59845c07aa030f0c81ee70184a90d35099a0e63"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+  end
 
   def install
     virtualenv_install_with_resources
