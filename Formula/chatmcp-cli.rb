ef5d2e65395a81903b554c2aa64c7c06d457fcd5
soulful-admin <soulful@soulless.app>
2025-06-16 13:06:23 +0200

    Add pillow dependency

--- Formula/chatmcp-cli.rb
+++ Formula/chatmcp-cli.rb
@@ -89,6 +89,11 @@ class ChatmcpCli < Formula
     sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
   end
 
+  resource "pillow" do
+    url "https://files.pythonhosted.org/packages/af/cb/bb5c01fcd2a69335b86c22142b2bccfc3464087efb7fd382eee5ffc7fdf7/pillow-11.2.1.tar.gz"
+    sha256 "a64dd61998416367b7ef979b73d3a85853ba9bec4c2925f74e588879a58716b6"
+  end
+
   def install
     virtualenv_install_with_resources
   end
