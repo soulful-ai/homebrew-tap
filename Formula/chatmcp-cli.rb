9b16282c7777a25b4be1814a00010097b342adac
soulful-admin <soulful@soulless.app>
2025-06-16 12:03:14 +0200

    Fix formula - remove manual resource declarations

--- Formula/chatmcp-cli.rb
+++ Formula/chatmcp-cli.rb
@@ -9,21 +9,8 @@ class ChatmcpCli < Formula
 
   depends_on "python@3.12"
 
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/00/f5/b47bdb6db60175c1a96a085506b2a5bc0e9467241ff6ee95bbfd93d0b41a/packaging-24.2.tar.gz"
-    sha256 "c228a6dc5e932d346bc5739379109d49e8853dd8223571c7c5b55260edc0b97f"
-  end
-
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/ad/7c/c3ebf7de8f0a0c3d7fd0ddfff0bbf7a9ea38b0e63cb0cc8b9c37eaab0cd3/setuptools-76.0.0.tar.gz"
-    sha256 "dc6c3b5e5e66c3195e71fa416b13aca1dd4066fb6fbe8b43e3ecd81b0e1b0b6d"
-  end
-
   def install
-    virtualenv_install_with_resources do
-      # Upgrade pip to ensure we can install all dependencies
-      system libexec/"bin/pip", "install", "--upgrade", "pip"
-    end
+    virtualenv_install_with_resources
   end
 
   test do
